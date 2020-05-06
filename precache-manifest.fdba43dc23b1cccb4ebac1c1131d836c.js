8c112c80920d7825fb560f60b909411d1b5bea74
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-06 22:46:56 +0530

    Updates

---
+++ precache-manifest.fdba43dc23b1cccb4ebac1c1131d836c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb5a7ced2f93f4c32afa9a56adcaddd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b90a5619817ddd33225",
+    "url": "./static/js/2.3c0eb7d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3c0eb7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0820371db8483386b5",
+    "url": "./static/js/main.b0e6c264.chunk.js"
+  },
+  {
+    "revision": "5df4354e9160e387b203",
+    "url": "./static/js/runtime-main.cfef89e9.js"
+  }
+]);

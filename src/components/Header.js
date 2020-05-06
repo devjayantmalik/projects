ec1bddf9b9ab3b747b4cf9c4a18502d658ac85ad
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-06 16:46:15 +0530

    updated header component to semantic html tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,12 @@ import React from "react";
 import {Link} from 'react-router-dom';
 
 const Header = () => (
-    <div className="ui menu">
-        <div className="header item">
-        <Link to="/">Jayant Malik</Link>
-        </div>
+    <header className="ui stackable menu">
+        <Link to="/" className="header item">Jayant Malik</Link>
         <Link to="/about" className="item">About Me</Link>
         <Link to="/projects" className="item">Projects</Link>
         <Link to="/contact" className="item">Contact</Link>
-    </div>
+    </header>
 );
 
 export default Header;

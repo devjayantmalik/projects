104f44e2ce3e1343c509fb07d09e9749b290f58e
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-06 16:46:30 +0530

    created demo home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Home = () => {
-    return <h1>Home</h1>
+import Language from "./Language";
+
+class Home extends React.Component {
+    renderLanguages = () => {
+        return [1, 2, 3, 4, 5, 6].map((lang) => <Language />);
+    };
+
+    render() {
+        return (
+            <main className="ui basic container">
+                <section className="pusher">
+                    <h2>Languages</h2>
+                    <div className="ui link cards">
+                        {this.renderLanguages()}
+                    </div>
+                </section>
+            </main>
+        );
+    }
 }
-
 
 export default Home;
